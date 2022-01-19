a4033d303c1d79eaa9f3e9d57de4c50bb855abe8
deploy <deploy@users.noreply.github.com>
2022-01-19 15:12:08 +0000

    Updated

--- wds@1.0.6/index.js
+++ wds@1.0.6/index.js
@@ -1,7 +1,9 @@
 // src/index.tsx
 import { default as default2 } from "./Button/Button.js";
 import { default as default3 } from "./Divider/Divider.js";
+import { default as default4 } from "./Avatar/Avatar.js";
 export {
+  default4 as Avatar,
   default2 as Button,
   default3 as Divider
 };
