70b390f92c5b9b22f51bfa108889ac3ce2bb1378
권종훈 <jounghun.kwon@wadiz.kr>
2022-01-20 13:35:40 +0900

    Input add

---
+++ src/Input/Input.tsx
@@ -0,0 +1,49 @@
+import React, { forwardRef } from 'react';
+import cx from 'classnames';
+
+import styles from './Input.module.scss';
+
+export interface InputProps<T = HTMLInputElement> extends Omit<React.InputHTMLAttributes<T>, 'size' | 'status'> {
+  /** Input Type */
+  type?: 'text' | 'number' | 'email' | 'password' | 'tel';
+
+  /** Input 크기 */
+  size?: 'sm' | 'md' | 'lg';
+
+  /** Input 상태 */
+  status?: 'error';
+  
+  className?: string;
+}
+
+export const Input = forwardRef<HTMLInputElement, InputProps>(({
+  type = 'text',
+  size = 'md',
+  status,
+  className,
+  ...props
+}, ref) => {
+  const classes = cx(
+    styles.input,
+    size && {
+      [styles.sm]: size === 'sm',
+      [styles.md]: size === 'md',
+      [styles.lg]: size === 'lg',
+    },
+    {
+      [styles.error]: status === 'error',
+    },
+    className
+  );
+  return (
+    <input
+      {...props}
+      type={type}
+      className={classes}
+      ref={ref}
+      aria-invalid={status === 'error' ? "true" : "false"}
+    />
+  );
+});
+
+export default Input;
--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,4 @@
 export { default as Button } from "./Button/Button";
 export { default as Divider } from "./Divider/Divider";
 export { default as Avatar } from "./Avatar/Avatar";
+export { default as Input } from "./Input/Input";
